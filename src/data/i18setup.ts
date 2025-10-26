6f4a62893661e138d35cea99aa9a13c54d570751
David Čadež <david.cadez89@gmail.com>
2025-10-26 15:42:50 +0100

    Removed de, hr and sr locales and fixed setup.

--- src/data/i18setup.ts
+++ src/data/i18setup.ts
@@ -1,19 +1,14 @@
 import {createI18n} from 'vue-i18n'
 import en from '@/locales/en.json'
-import de from '@/locales/de.json'
 import sl from '@/locales/sl.json'
-import hr from '@/locales/hr.json'
-import sr from '@/locales/sr.json'
 
 const i18n = createI18n({
+    legacy: true,
     locale: 'sl',
-    fallbackLocale: 'de',
+    fallbackLocale: 'en',
     messages: {
         en,
-        de,
         sl,
-        hr,
-        sr
     },
     globalInjection: true
 })
